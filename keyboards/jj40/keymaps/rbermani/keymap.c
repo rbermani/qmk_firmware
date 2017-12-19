d893ae6f2cb056acba521c3a03a77a9a20a2f699
rbermani <bobbermani@gmail.com>
2017-12-19 15:16:00 -0500

    keymap change

--- keyboards/jj40/keymaps/rbermani/keymap.c
+++ keyboards/jj40/keymaps/rbermani/keymap.c
@@ -37,50 +37,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  OS  | Alt  |   `  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |  OS  | Alt  |Lower | Raise|    Space    |   `  | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,       KC_T,    KC_Y, KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,       KC_G,    KC_H, KC_J,       KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,  KC_V,       KC_B,    KC_N, KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-  KC_LCTL, KC_LGUI, KC_LALT, KC_GRV, TT(_LOWER), KC_SPC,        TT(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,       KC_R,       KC_T,    KC_Y, KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,       KC_F,       KC_G,    KC_H, KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,       KC_V,       KC_B,    KC_N, KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
+  KC_LCTL, KC_LGUI, KC_LALT, TT(_LOWER), TT(_RAISE), KC_SPC,        KC_GRV, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * | Caps |   &  |   *  |   _  |   (  |   )  |   =  |   7  |   8  |   9  |   |  |  Del |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  | 
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   $  |   %  |   ^  |   [  |   ]  |   -  |   4  |   5  |   6  |   +  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   !  |   @  |   #  |   {  |   }  |      |   1  |   2  |   3  |   \  |      |
+ * | Ins  |      |      |      |      |      | Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |PrScr | Back | Fwd  |      |      |      |      |      | Mute | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  OS  | Alt  |   ~  | Lower|      0      |Raise |   0  |   .  |      | End  |
+ * | Ctrl |  OS  | Alt  |Lower | Lock |             |   `  | Prev | Stop | Play | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = KEYMAP( \
-  KC_CAPS, KC_AMPR, KC_ASTR, KC_UNDS, KC_LPRN, KC_RPRN, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_PIPE, KC_DEL, \
-  KC_TAB,  KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_MINS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_ENT, \
-  KC_LSFT, KC_EXLM, KC_AT,   KC_HASH, KC_LCBR, KC_RCBR, _______, KC_1,    KC_2,    KC_3,    KC_BSLS, _______, \
-  _______, _______, _______, KC_TILD, _______, KC_SPC,           _______, KC_0,    KC_DOT,  _______, KC_END \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+  KC_INS,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,\
+  KC_PSCR, KC_WBAK, KC_WFWD, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,\
+  _______, _______, _______, _______, KC_LOCK, _______,          _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * | Caps |   &  |   *  |   _  |   (  |   )  |   =  |   7  |   8  |   9  | Del  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  | PgUp |      |      |      | Enter|
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |PrScr |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | PgDn | Mute | Vol- | Vol+ |      |
+ * | Tab  |   $  |   %  |   ^  |   [  |   ]  |   -  |   4  |   5  |   6  |   +  |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   !  |   @  |   #  |   {  |   }  |   .  |   1  |   2  |   3  |   \  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  OS  | Alt  |Lock  |Lower |             | Raise| Prev | Stop | Play | Next |
+ * | Ctrl |  OS  | Alt  | Lower| Raise|             |   0  | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PGUP, _______, _______, _______, KC_ENT, \
-  KC_PSCR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGDN, KC_MUTE, KC_VOLD, KC_VOLU, _______, \
-  _______, _______, _______, KC_LOCK, _______, _______,          _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT \
+  KC_CAPS, KC_AMPR, KC_ASTR, KC_UNDS, KC_LPRN, KC_RPRN, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_DEL,  KC_BSPC, \
+  _______, KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_MINS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_ENT,  \
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_LCBR, KC_RCBR, KC_DOT,  KC_1,    KC_2,    KC_3,    KC_BSLS, KC_PIPE, \
+  _______, _______, _______, _______, _______, _______,          KC_0,    KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
 )
-
 };
